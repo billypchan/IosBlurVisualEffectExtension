a62fbac7a0cef44b86c09f372f36b69bee853071
Bill Chan MBA <billypchan@gmail.com>
2016-07-01 23:05:30 +0200

    tested side-effect to other animations

--- testBlurAnimation/ViewController.swift
+++ testBlurAnimation/ViewController.swift
@@ -37,7 +37,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        blurview.effect = nil
+        blurview.center = CGPoint(x: blurview.center.x, y: blurview.center.y+blurview.frame.size.height)
+
+
+                blurview.effect = nil
+        UIView.animateWithDuration(0.5, delay: 0.5, options:UIViewAnimationOptions.CurveLinear, animations: {
+            self.blurview.center = self.view.center
+            }, completion: {
+                (value: Bool) in
+                        self.blurEffectView(enable: true, blurView: self.blurview, percentage:0.5)
+        })
+        
+    }
+    
+    ///The Hack works even the view is hidden
+    func testBlurWhenHidden() {
+        blurview.hidden = true
+        blurEffectView(enable: true, blurView: blurview, percentage:0.7)
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(10 * Double(NSEC_PER_SEC)))
+        
+        dispatch_after(delayTime, dispatch_get_main_queue()) {
+            self.blurview.hidden = false
+        }
+    }
+    
+    func testBlurInDifferentLevel(){
         blurEffectView(enable: true, blurView: blurview, percentage:0.3)
         
         delayedUndoBlurEffect(2)
@@ -48,7 +72,7 @@ class ViewController: UIViewController {
         delayedUndoBlurEffect(11)
         delayedBlurEffect(13, percentage:1.0)
     }
-
+    
     func delayedBlurEffect(time:Double, percentage:Double) {
         let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(time * Double(NSEC_PER_SEC)))
         
@@ -56,7 +80,7 @@ class ViewController: UIViewController {
             self.blurEffectView(enable: true, blurView: self.blurview, percentage:percentage)
         }
     }
-
+    
     func delayedUndoBlurEffect(time:Double) {
         let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(time * Double(NSEC_PER_SEC)))
         
@@ -65,6 +89,7 @@ class ViewController: UIViewController {
         }
     }
     
+    ///NOTICE: this func will pause all other animations! Match the pause time with other animation,
     func blurEffectView(enable enable: Bool, blurView: UIVisualEffectView!, percentage:Double) {
         let animationTime = 0.5
         
